b023e56a6fccf071eae29a6de71036d6b1f206ba
janhavinaidu <janhavinaidu123@gmail.com>
2026-05-26 08:49:43 +0530

    Initial commit

---
+++ src/components/Cards.jsx
@@ -0,0 +1,9 @@
+function Cards(){
+    return(
+        <div className="c">
+            <img classname="c-img" src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FybHN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60" alt=""/>
+
+            <h4 className="Scenary">Cards</h4>
+        </div>
+    )
+}
